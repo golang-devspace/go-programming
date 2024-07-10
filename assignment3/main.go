8ecb7f9a0a751fca600b3de481d0b7f77c89d078
akashrajguru <akash.rajguru@est.tech>
2024-07-10 21:21:55 +0100

    assignment3 io and os package

--- assignment3/main.go
+++ assignment3/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, f)
 }
